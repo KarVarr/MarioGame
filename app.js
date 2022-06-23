9e2760fd6ac5b8654869646ec132b8812a981cc8
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-06-23 17:39:23 +0300

    ver 0.2

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const btn = document.querySelector(".btn__button");
 const mario = document.querySelector(".imgMario");
+const mashroom = document.querySelector(".img__mush");
 
-btn.addEventListener("click", () => {
-  // removeActiveClasses()
-  mario.classList.toggle("active");
-});
 
-function removeActiveClasses() {
-  btn.addEventListener("click", () => {
-    mario.classList.remove("active");
-  });
+const marioTop = parseInt(
+  window.getComputedStyle(mario).getPropertyValue("top")
+);
+const mushroomLeft = parseInt(
+  window.getComputedStyle(mashroom).getPropertyValue("left")
+);
+
+function jump () {
+    btn.addEventListener("click", () => {
+     if(mario.classList != 'active'){
+      mario.classList.add("active");
+     }
+      setTimeout(function(){
+         mario.classList.remove("active");
+      }, 500)
+    });
 }
+jump ()
 
-// btn.addEventListener("click", () => {
-//     remove();
-//     mario.classList.add("active");
-//   });
+ 
+const check = setInterval(function () {
 
-// function remove() {
-
-//     mario.classList.remove("active");
-
-//   };
+console.log(marioTop, mushroomLeft);
+    if (mushroomLeft < 1641 && mushroomLeft > 0 && marioTop >= 1700) {
+      alert(`You'r lose!`);
+    }
+},10)   
